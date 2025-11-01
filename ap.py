185aca4dc40587fe2f4fcc635d1f679badb9be83
Min LI <minli@Mins-MacBook-Air.local>
2025-11-01 15:43:21 -0400

    modified:   ap.py

--- ap.py
+++ ap.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, Response, jsonify, request
 import image_detect_api 
 import threading
 # Create Flask app
